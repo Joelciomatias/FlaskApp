89c447264cda686492c631a011bb0a0078fd8b5b
joelcio <joelcio_psx@hotmail.com>
2019-10-09 22:56:55 -0300

    adicionado test numpy

--- common/app/views.py
+++ common/app/views.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_celery import make_celery
 from flask_sqlalchemy import SQLAlchemy
 from random import choice
+from test_numpy import test_py
 
 app = Flask(__name__)
 #broker='amqp://localhost//',backend='db+mysql+pymysql://root:password@localhost:3306/search')ap
@@ -14,7 +15,7 @@ db = SQLAlchemy(app)
 class Results(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     data = db.Column('data',db.String(50))
-print(soma_args(3,4,4,4,4))
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -29,6 +30,11 @@ def insertData():
     insert.delay()
     return 'i sent a async request into database'
 
+@app.route('/test-python')
+def test_python_task(iterations=1):
+    test_python.delay(iterations)
+    return 'I sent a test python async!'
+
 @celery.task(name='views.reverse')
 def reverse(string):
     return string[::-1]
@@ -41,6 +47,10 @@ def insert():
         db.session.add(result)
     db.session.commit()
     return 'Foi!!!'
+
+@celery.task(name='views.test_python')
+def test_python(interations):
+    return test_py(interations)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True)
